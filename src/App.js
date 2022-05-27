9f5dabb408d5037ece4d544fad4895a5ffe8083e
leejiyoung <103088450+jiyounggo@users.noreply.github.com>
2022-05-27 23:26:39 +0900

    detail-route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Routes,Route,Link } from 'react-router-dom';
+import Detail from './routes/detail.js';
+import Cart from  './routes/cart.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     메인
+    <Routes>
+      <Route path="/detail0" element={<Detail/> } />
+      <Route path="/cart" element={ <Cart/> } />
+    </Routes>
     </div>
   );
 }
---
+++ src/routes/cart.js
@@ -0,0 +1,17 @@
+import { Table } from 'react-bootstrap'
+import styled from 'styled-components'
+
+let YellowBtn = styled.button`
+  background : yellow;
+  color : black;
+  padding : 10px;
+`;
+
+function Cart(){
+    return(
+        <YellowBtn>버튼임</YellowBtn>
+    )
+}
+
+
+export default Cart
