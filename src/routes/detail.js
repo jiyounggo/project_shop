75bb85849319f2cfdbe96c250ca8a43553d0a353
leejiyoung <103088450+jiyounggo@users.noreply.github.com>
2022-05-28 22:37:34 +0900

    comment function

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,14 +1,26 @@
-import { useState } from 'react';
+import { useState ,useEffect} from 'react';
 import { Link } from 'react-router-dom';
-import {Nav} from 'react-bootstrap'
+import {Nav,Card,ListGroup} from 'react-bootstrap'
 
 function Detail(){
   let [modal, setModal] = useState(true);
   let [탭 , 탭변경]=useState(0);
+  let [alert, setAlert] = useState(true);
 
+  useEffect(()=>{
+    setTimeout(()=>{ setAlert(false) }, 2000)
+  }, [])
 
     return ( 
+     
         <div className="container">
+           {
+        alert == true
+        ? <div className="alert alert-warning">
+          로딩중
+          </div>
+        : null
+      }
         <div className="row">
           <div className="col-md-6">
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
@@ -57,17 +69,50 @@ function Detail(){
 
       </div> 
 
-      
+
     )
   }
 
   function Tabcontent(props){
+    let [입력값,입력값변경]=useState('');
+    let [글제목,글제목변경]=useState(['상품 너무 좋네요 호호','맛집이네~','존맛탱']);
+
     if(props.탭==0){
       return <div>내용0</div>
     }if(props.탭==1){
       return <div>내용1</div>
     }if(props.탭==2){
-      return <div>내용2</div>
+      return <div> 
+       <form onSubmit={(e)=>{
+         e.preventDefault();
+         
+       }}>
+      <input onChange={(e)=>{입력값변경(e.target.value)}}></input>
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy.unshift(입력값+'X');
+        글제목변경(copy);
+      }}>댓글</button>
+       </form>
+  {  
+        글제목.map(function(a, i){
+          return (
+          <div className="list">
+            <h4>{ 글제목[i] }</h4>
+            <button onClick={()=>{ 
+              let copy = [...글제목];
+              copy.splice(i, 1);
+              글제목변경(copy);
+           }}>삭제</button>
+         
+          </div> 
+          )
+        }) 
+      
+}
+
+
+      </div>
     }if(props.탭==3){
       return <div>내용3</div>
   }
